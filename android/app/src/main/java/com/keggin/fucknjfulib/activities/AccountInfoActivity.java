7669e4d4aa41e6fa603759e2a14efbab384c4cba
keggin <109941504+keggin-CHN@users.noreply.github.com>
2026-09-20 13:09:46 +0800

    fix(android): add missing import android.util.Log in AccountInfoActivity

--- android/app/src/main/java/com/keggin/fucknjfulib/activities/AccountInfoActivity.java
+++ android/app/src/main/java/com/keggin/fucknjfulib/activities/AccountInfoActivity.java
@@ -1,5 +1,6 @@
 package com.keggin.fucknjfulib.activities;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
